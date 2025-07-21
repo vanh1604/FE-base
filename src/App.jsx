5714bf853d149da55f07b86884970785e4f2d3c7
leh19923 <your-email@example.com>
2025-07-21 23:11:35 +0700

    done screen contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Login from "./pages/Login";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      
+
         <Route path="/" element={<LayoutApp />}>
           <Route path='login' element={<Login />} />
           <Route index element={<Home />} />
           <Route path="collection" element={<Collection />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
